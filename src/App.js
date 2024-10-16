e7737017be16c1e0722e2f5c95a81da395584bcb
Raymark Vergara <raymarksevillavergara@gmail.com>
2024-10-17 01:36:34 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+import { ToastContainer } from "react-toastify"; //
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+        <ToastContainer />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
